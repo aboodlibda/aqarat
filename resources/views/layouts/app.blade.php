d9caaa778bc62e0583a214d169614cb87378e0db
ABDULLHA YOUSEF <52853179+aboodlibda@users.noreply.github.com>
2022-05-13 20:56:22 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <!--stylesheet include-->
     <link rel="stylesheet" type="text/css" media="all" href="{{asset('front/plugins/layerslider/css/layerslider.css')}}">
+    <link rel="stylesheet" type="text/css" media="all" href="{{asset('front/plugins/layerslider/skins/defaultskin/skin.css')}}">
     <link rel="stylesheet" type="text/css" media="all" href="{{asset('front/plugins/owl-carousel/assets/owl.carousel.min.css')}}">
     <link rel="stylesheet" type="text/css" media="all" href="{{asset('front/plugins/fancybox/jquery.fancybox.css')}}">
     <link rel="stylesheet" type="text/css" media="all" href="{{asset('front/plugins/jackbox/css/jackbox.min.css')}}">
